959fd14960cfc3570ab6d7f918b48bb041edbc43
Bohdan K <bohdan.kobel.2@aptiv.com>
2023-11-16 12:01:14 +0100

    logger benchmark

---
+++ src/PartialVsLogger/StringConcatenationBenchmark.cs
@@ -0,0 +1,42 @@
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
+
+[MemoryDiagnoser]
+public class StringConcatenationBenchmark
+{
+    private const int ExecTimes = 1000 * 1000 * 100;
+
+    private static ILogger _logger = LoggerFactory.Create(builder => builder.AddDebug()).CreateLogger("Default");
+    
+    [Benchmark]
+    public void LogSimple()
+    {
+        for (var i = 0; i < ExecTimes; i++)
+        {
+            var count = 10;
+            var address = 123123123L;
+            var message = "Moving";
+
+            _logger.LogInformation("{Message} {Count} items from {Address}", message, count, address);
+        }
+    }
+
+    [Benchmark]
+    public void ConcatWithStringBuilder()
+    {
+        for (var i = 0; i < ExecTimes; i++)
+        {
+            var count = 10;
+            var address = 123123123L;
+            var message = "Moving";
+
+            _logger.LogPeriodicWorkFailure(message, count, address);
+        }
+    }
+}
+
+public static partial class ConsoleLogger
+{
+    [LoggerMessage(LogLevel.Information, "{Message} {Count} items from {Address}")]
+    public static partial void LogPeriodicWorkFailure(this ILogger logger, string message, int count, long address);
+}
